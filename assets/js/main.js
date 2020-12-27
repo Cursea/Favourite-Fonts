7f9db157918bf8ae0da73792dcfb62168bf46fb4
Marco <marcoburman@gmail.com>
2020-12-27 03:25:15 +0000

    Push final note comments before moving on to React conversion

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,12 @@ function updateValue(e) {
     element.textContent = e.target.value
   })
 }
+
+//null out the value of sample text in the HTML
+/* think about adding
+  if !(customText) {
+    sample = "Some cool string of text, possibly philosophical"
+  }
+*/
+//but also consider maybe leaving the sample text empty after user
+//deletes input, and only set up default sample text if Reset is clicked
